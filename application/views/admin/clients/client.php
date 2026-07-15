e70207f7b6f017305c7614d1036a73f8e51f9f70
AnkithaC16 <ankithaachar16@gmail.com>
2026-07-15 17:51:14 +0530

    Updated customer model and ui

--- application/views/admin/clients/client.php
+++ application/views/admin/clients/client.php
@@ -96,7 +96,7 @@
                 <?php } ?>
 
                 <div class="panel_s">
-                    <?php if (isset($client) && $group == 'profile') { ?>
+                    <!-- <?php if (isset($client) && $group == 'profile') { ?>
                     <div class="panel-heading" style="display:flex;justify-content:flex-end;padding:10px 15px;">
                         <button type="button" class="btn btn-info btn-sm" id="btn-edit-client" onclick="enableClientEditMode();">
                             <i class="fa fa-pencil"></i> Edit
@@ -105,7 +105,8 @@
                             <i class="fa fa-times"></i> Cancel
                         </button>
                     </div>
-                    <?php } ?>
+                    <?php } ?> -->
+                   
                     <div class="panel-body">
                         <?php if (isset($client)) { ?>
                         <?= form_hidden('isedit'); ?>
@@ -117,9 +118,92 @@
                                 <?php $this->load->view((isset($tab) ? $tab['view'] : 'admin/clients/groups/profile')); ?>
                             </div>
                         </div>
+                        <hr>
+
+<div class="client-action-bar">
+
+    <div class="pull-left">
+
+        <a href="<?= admin_url('clients/client'); ?>"
+           class="btn btn-default">
+            <i class="fa fa-plus"></i> New
+        </a>
+
+        <a href="<?= admin_url('clients'); ?>"
+           class="btn btn-default">
+            <i class="fa fa-list"></i> View
+        </a>
+
+    </div>
+
+    <?php if(isset($client)){ ?>
+
+    <div class="pull-right">
+
+        <button
+            type="button"
+            class="btn btn-primary"
+            id="btn-edit-client"
+            onclick="enableClientEditMode();">
+
+            <i class="fa fa-pencil"></i> Edit
+
+        </button>
+
+        <button
+            type="button"
+            class="btn btn-success"
+            id="btn-save-client"
+            style="display:none;"
+            onclick="$('.only-save').click();">
+
+            <i class="fa fa-save"></i> Save
+
+        </button>
+
+        <button
+            type="button"
+            class="btn btn-warning"
+            id="btn-clear-client"
+            style="display:none;"
+            onclick="clearClientForm();">
+
+            <i class="fa fa-eraser"></i> Clear
+
+        </button>
+
+        <button
+            type="button"
+            class="btn btn-default"
+            id="btn-cancel-edit-client"
+            style="display:none;"
+            onclick="disableClientEditMode();">
+
+            Cancel
+
+        </button>
+
+        <?php if(staff_can('delete','customers')){ ?>
+
+        <a href="<?= admin_url('clients/delete/'.$client->userid); ?>"
+           class="btn btn-danger _delete">
+
+            Delete
+
+        </a>
+
+        <?php } ?>
+
+    </div>
+
+    <?php } ?>
+
+    <div class="clearfix"></div>
+
+</div>
                     </div>
                     <?php if ($group == 'profile') { ?>
-                    <div class="panel-footer text-right tw-space-x-1" id="profile-save-section" <?php if (isset($client)) { ?>style="display:none;"<?php } ?>>
+                    <div class="panel-footer text-right tw-space-x-1" id="profile-save-section" style="display:none;">
                         <?php if (! isset($client)) { ?>
                         <button class="btn btn-default save-and-add-contact customer-form-submiter">
                             <?= _l('save_customer_and_add_contact'); ?>
@@ -129,6 +213,39 @@
                             <?= _l('submit'); ?>
                         </button>
                     </div>
+                     <!-- <?php if ($group == 'profile') { ?>
+                    <div class="panel-footer text-right tw-space-x-1" id="profile-save-section" <?php if (isset($client)) { ?>style="display:none;"<?php } ?>>
+                    <div class="panel-footer text-right tw-space-x-1" id="profile-save-section" style="display:none;">
+                        <?php if (! isset($client)) { ?>
+                        <button class="btn btn-default save-and-add-contact customer-form-submiter">
+                        <button class="btn btn-default save-and-add-contact customer-form-submiter" style="display:none;">
+                            <?= _l('save_customer_and_add_contact'); ?>
+                        </button>
+                        <?php } ?>
+                        <button class="btn btn-primary only-save customer-form-submiter">
+                        <button class="btn btn-primary only-save customer-form-submiter" style="display:none;">
+                            <?= _l('submit'); ?>
+                        </button>
+                    </div>
+                    <div class="panel-footer text-center tw-space-x-2" style="display: flex; justify-content: center; gap: 10px;">
+                        <a href="<?= admin_url('clients/client'); ?>" class="btn btn-primary">New</a>
+                        <?php if (isset($client)) { ?>
+                            <button type="button" class="btn btn-info" onclick="enableClientEditMode();">Edit</button>
+                        <?php } else { ?>
+                            <button type="button" class="btn btn-info" disabled>Edit</button>
+                        <?php } ?>
+                        <a href="<?= admin_url('clients'); ?>" class="btn btn-success">View</a>
+                        <button type="button" class="btn btn-warning" onclick="$('.customer-form-submiter.only-save').click();">Save</button>
+                        <a href="<?= admin_url('clients/client' . (isset($client) ? '/' . $client->userid : '')); ?>" class="btn btn-default">Clear</a>
+                        <?php if (isset($client) && staff_can('delete', 'customers')) { ?>
+                            <a href="<?= admin_url('clients/delete/' . $client->userid); ?>" class="btn btn-danger _delete">Delete</a>
+                        <?php } else { ?>
+                            <button type="button" class="btn btn-danger" disabled>Delete</button>
+                        <?php } ?>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div> -->
                     <?php } ?>
                 </div>
             </div>
@@ -149,27 +266,73 @@
         <?php } ?>
     });
 
+    // function enableClientEditMode() {
+    //     var $form = $('.client-form');
+    //     $form.find('input:not([type=hidden]), textarea, select').prop('disabled', false);
+    //     // Re-initialize selectpicker dropdowns after enabling
+    //     $form.find('select.selectpicker').selectpicker('refresh');
+
+    //     $('#profile-save-section').slideDown(200);
+    //     $('#btn-edit-client').hide();
+    //     $('#btn-cancel-edit-client').show();
+    // }
+
+    // function disableClientEditMode() {
+    //     var $form = $('.client-form');
+    //     $form.find('input:not([type=hidden]), textarea, select').prop('disabled', true);
+    //     // Re-initialize selectpicker dropdowns after disabling
+    //     $form.find('select.selectpicker').selectpicker('refresh');
+
+    //     $('#profile-save-section').slideUp(200);
+    //     $('#btn-edit-client').show();
+    //     $('#btn-cancel-edit-client').hide();
+    // }
     function enableClientEditMode() {
-        var $form = $('.client-form');
-        $form.find('input:not([type=hidden]), textarea, select').prop('disabled', false);
-        // Re-initialize selectpicker dropdowns after enabling
-        $form.find('select.selectpicker').selectpicker('refresh');
 
-        $('#profile-save-section').slideDown(200);
-        $('#btn-edit-client').hide();
-        $('#btn-cancel-edit-client').show();
-    }
+    var $form = $('.client-form');
 
-    function disableClientEditMode() {
-        var $form = $('.client-form');
-        $form.find('input:not([type=hidden]), textarea, select').prop('disabled', true);
-        // Re-initialize selectpicker dropdowns after disabling
-        $form.find('select.selectpicker').selectpicker('refresh');
+    $form.find('input:not([type=hidden]), textarea, select')
+         .prop('disabled', false);
 
-        $('#profile-save-section').slideUp(200);
-        $('#btn-edit-client').show();
-        $('#btn-cancel-edit-client').hide();
-    }
+    $form.find('select.selectpicker').selectpicker('refresh');
+
+    //$('#profile-save-section').show();
+
+    $('#btn-edit-client').hide();
+    $('#btn-save-client').show();
+    $('#btn-clear-client').show();
+    $('#btn-cancel-edit-client').show();
+}
+
+function disableClientEditMode() {
+
+    var $form = $('.client-form');
+
+    $form.find('input:not([type=hidden]), textarea, select')
+         .prop('disabled', true);
+
+    $form.find('select.selectpicker').selectpicker('refresh');
+
+    //$('#profile-save-section').hide();
+
+    $('#btn-edit-client').show();
+    $('#btn-save-client').hide();
+    $('#btn-clear-client').hide();
+    $('#btn-cancel-edit-client').hide();
+}
+
+function clearClientForm() {
+
+    $('.client-form').find(
+        'input:not([type=hidden],[type=button],[type=submit],[type=reset])'
+    ).val('');
+
+    $('.client-form textarea').val('');
+
+    $('.client-form select').prop('selectedIndex', 0);
+
+    $('.client-form .selectpicker').selectpicker('refresh');
+}
 </script>
 <?php } ?>
 <?php $this->load->view('admin/clients/client_js'); ?>
